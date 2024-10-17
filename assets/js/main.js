860a814b9011b7d069041191760c8a213d2adc2f
Nicolas Vargas <nicolasvargaschiqiza@gmail.com>
2024-10-17 09:50:23 -0500

    Cambios en Main y .json

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,6 +41,21 @@ document.addEventListener("DOMContentLoaded", () => {
                 let cards = '';
 
                 tab.projects.forEach(project => {
+                    // Generamos dinámicamente los elementos de tecnología
+                    let tecnologiasHTML = '';
+                    if (project.modal && project['modal-title']) {
+                        Object.keys(project.modal).forEach((key, index) => {
+                            const icono = project.modal[`icon${index + 1}`];
+                            const titulo = project['modal-title'][`title${index + 1}`];
+                            tecnologiasHTML += `
+                                <div class="tecnologias-desc mx-2">
+                                    <h6>${titulo}</h6>
+                                    <i class="fa-brands ${icono} fa-2x"></i>
+                                </div>
+                            `;
+                        });
+                    }
+
                     cards += `
                     <div class="card mb-3 wow animate__animated animate__fadeIn" data-wow-delay="${project.time}">
                         <div class="card-body">
@@ -50,10 +65,44 @@ document.addEventListener("DOMContentLoaded", () => {
                                 <p class="card-text">${project.description}</p>
                             </div>
                         </div>
- 
-                        <button type="button" data-bs-toggle="modal" data-bs-target="#Modal${project.id}" class="card-button fw-bold">Ver proyecto</button>
+
+                        <button type="button" data-bs-toggle="modal" data-bs-target="#Modal${project.nModal + project.id}" class="card-button fw-bold">Ver proyecto</button>
                     </div>
 
+                    <!-- Modal -->
+                    <div class="modal fade" id="Modal${project.nModal + project.id}" tabindex="-1" aria-hidden="true">
+                        <div class="modal-dialog modal-lg modal-dialog-centered">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="btn-close btn-modal-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="card-modal">
+                                        <div class="card-modal-details">
+                                            <div class="row">
+                                                <div class="col-12 col-lg-6 align-self-center">
+                                                    <img src="./assets/img/${project.img}" class="img-fluid"
+                                                        alt="Proyectos monodual ${project.title}">
+                                                </div>
+                                                <div class="col-12 col-lg-6">
+                                                    <h1>${project.title}</h1>
+                                                    <p>${project.description}</p>
+                                                    <br>
+                                                    <div class="tecnologias text-center">
+                                                        <h2>Tecnologías</h2>
+                                                        <div class="tecnologias-icons d-flex justify-content-center">
+                                                            ${tecnologiasHTML} <!-- Aquí se inserta el HTML generado -->
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <a href="#" class="card-modal-button">More info</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     `;
                 });
 
